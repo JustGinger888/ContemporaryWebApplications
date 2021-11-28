450ae5ef376795d2c1e072ac1a09984afb0563f1
Ethan Moffatt <ethanmoffatt@gmail.com>
2021-11-28 20:35:06 +0000

    upd

--- deliversity/components/Loader.js
+++ deliversity/components/Loader.js
@@ -2,7 +2,7 @@
 export default function Loader({ show }) {
     return show ? <div class="flex justify-center items-center">
     <div
-      class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"
+      class="animate-spin rounded-full h-32 w-32 border-b-2 border-red-500"
     ></div>
   </div> : null;
   }
