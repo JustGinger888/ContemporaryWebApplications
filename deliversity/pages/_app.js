a6efc3d3daa6c2679ee8f857c1041ad3af6cd26f
Ethan Moffatt <ethanmoffatt@gmail.com>
2022-01-11 23:00:29 +0000

    upd

--- deliversity/pages/_app.js
+++ deliversity/pages/_app.js
@@ -1,6 +1,6 @@
 import "tailwindcss/tailwind.css";
-import Navbar from "../components/Navbar.js";
-import Footer from "../components/Footer.js";
+import Navbar from "../Components/Navbar.js";
+import Footer from "../Components/Footer.js";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
 import { useUserData } from "../lib/hooks";
